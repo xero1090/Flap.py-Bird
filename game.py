37f2c93f4f093908b390d3c9dcf12a25af3185f5
Xero1090 <49799244+xero1090@users.noreply.github.com>
2019-05-01 19:23:38 -0400

    updated, lets make changes to it

--- game.py
+++ game.py
@@ -1,4 +1,4 @@
-# Flappy bird by Kevin Tran 991566232
+# Flappy bird by Kevin Tran, (add your name here)
 # 5/1/2019
 
 import pygame
@@ -6,28 +6,101 @@ from pygame.locals import *
 import sys
 import random
 
-class bird:
+class Bird:
     def __init__(self):
         self.screen = pygame.display.set_mode((400, 708))
         # Stores rectangular coordinates
         self.bird = pygame.Rect(65, 50, 50, 50)
         # Loads background Image and changes the pixel format of an image
         self.background = pygame.image.load("images/background.png").convert()
-        # Loads bird sprites and changes the pixel format of an img including ppa (per pixel alphas)
-        self.birdsprites = [pygame.image.load("images/1.png").convert_alpha(),
+         # Loads bird sprites and changes the pixel format of an img including ppa (per pixel alphas)
+        self.birdSprites = [pygame.image.load("images/1.png").convert_alpha(),
                             pygame.image.load("images/2.png").convert_alpha(),
                             pygame.image.load("images/dead.png")]
-        self.topPipe = pygame.image.load("images/bottom.png").convert_alpha()
-        self.botPipe = pygame.image.load("images/top.png").convert_alpha()
-        self.space = 130
-        self.pipex = 400
+        self.wallUp = pygame.image.load("images/bottom.png").convert_alpha()
+        self.wallDown = pygame.image.load("images/top.png").convert_alpha()
+        self.gap = 130
+        self.wallx = 400
         self.birdY = 350
-        self.fly = 0
-        self.flySpeed = 10
+        self.jump = 0
+        self.jumpSpeed = 10
         self.gravity = 5
         self.dead = False
         self.sprite = 0
         self.counter = 0
         self.offset = random.randint(-110, 110)
 
-        
+    def updateWalls(self):
+        self.wallx -= 2
+        if self.wallx < -80:
+            self.wallx = 400
+            self.counter += 1
+            self.offset = random.randint(-110, 110)
+
+    def birdUpdate(self):
+        if self.jump:
+            self.jumpSpeed -= 1
+            self.birdY -= self.jumpSpeed
+            self.jump -= 1
+        else:
+            self.birdY += self.gravity
+            self.gravity += 0.2
+        self.bird[1] = self.birdY
+        upRect = pygame.Rect(self.wallx,
+                             360 + self.gap - self.offset + 10,
+                             self.wallUp.get_width() - 10,
+                             self.wallUp.get_height())
+        downRect = pygame.Rect(self.wallx,
+                               0 - self.gap - self.offset - 10,
+                               self.wallDown.get_width() - 10,
+                               self.wallDown.get_height())
+        if upRect.colliderect(self.bird):
+            self.dead = True
+        if downRect.colliderect(self.bird):
+            self.dead = True
+        if not 0 < self.bird[1] < 720:
+            self.bird[1] = 50
+            self.birdY = 50
+            self.dead = False
+            self.counter = 0
+            self.wallx = 400
+            self.offset = random.randint(-110, 110)
+            self.gravity = 5
+
+    def run(self):
+        clock = pygame.time.Clock()
+        pygame.font.init()
+        font = pygame.font.SysFont("Arial", 50)
+        while True:
+            clock.tick(60)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+                if (event.type == pygame.KEYDOWN or event.type == pygame.MOUSEBUTTONDOWN) and not self.dead:
+                    self.jump = 17
+                    self.gravity = 5
+                    self.jumpSpeed = 10
+
+            self.screen.fill((255, 255, 255))
+            self.screen.blit(self.background, (0, 0))
+            self.screen.blit(self.wallUp,
+                             (self.wallx, 360 + self.gap - self.offset))
+            self.screen.blit(self.wallDown,
+                             (self.wallx, 0 - self.gap - self.offset))
+            self.screen.blit(font.render(str(self.counter),
+                                         -1,
+                                         (255, 255, 255)),
+                             (200, 50))
+            if self.dead:
+                self.sprite = 2
+            elif self.jump:
+                self.sprite = 1
+            self.screen.blit(self.birdSprites[self.sprite], (70, self.birdY))
+            if not self.dead:
+                self.sprite = 0
+            self.updateWalls()
+            self.birdUpdate()
+            pygame.display.update()
+
+if __name__ == "__main__":
+    Bird().run()
