55cc3347bb171700c7c4d4685d31ee14aaa8a88a
Xero1090 <49799244+xero1090@users.noreply.github.com>
2019-05-01 22:04:53 -0400

    Created new file (we will use this)
    The other file is a guide.

---
+++ flapgame.py
@@ -0,0 +1,31 @@
+# Flappy bird by Kevin Tran, (add your name here)
+# 5/1/2019
+
+import pygame
+from pygame.locals import *  
+import sys
+import random
+import os
+
+class Bird:
+    def __init__(self):
+        self.screen = pygame.display.set_mode((400, 708))
+        # Stores rectangular coordinates
+        self.bird = pygame.Rect(65, 50, 50, 50)
+        # Loads background Image and changes the pixel format of an image
+        self.background = pygame.image.load("images/background.png").convert()
+         # Loads bird sprites and changes the pixel format of an img including ppa (per pixel alphas)
+        self.birdSprites = [pygame.image.load("images/1.png").convert_alpha(),
+                            pygame.image.load("images/2.png").convert_alpha(),
+                            pygame.image.load("images/dead.png")]
+        self.wallUp = pygame.image.load("images/bottom.png").convert_alpha()
+        self.wallDown = pygame.image.load("images/top.png").convert_alpha()
+        self.death = False
+        
+    def move(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                os._exit(1)
+            keys = pygame.key.get_pressed()
+        for key in keys:
+            if keys[pygame.K_LEFT]:
